c7035215cd1ee090a4773c5d7d6df46535d857a9
HelligeChris <cringtved@gmail.com>
2021-09-27 09:55:45 +0200

    installable from git

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='DisplayMath',
+    url='https://github.com/HelligeChris/DisplayMath',
+    author='HelligeChris',
+    # Needed to actually package something
+    packages=['DisplayMath'],
+    # Needed for dependencies
+    install_requires=['sympy', 'IPython'],
+    # *strongly* suggested for sharing
+    version='0.0.1',
+    # The license can be anything you like
+    license='MIT',
+    description='Display latex in python',
+    # We will also need a readme eventually (there will be a warning)
+    long_description=open('README.md').read(),
+)
--- src/displayMath_package/__init__.py
+++
@@ -1,1 +0,0 @@
-from displayMath_package.displayMath import displayMath
